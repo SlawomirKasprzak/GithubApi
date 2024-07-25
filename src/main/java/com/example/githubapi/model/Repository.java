392af951cb73c2ff17146da877c257b6ed81d71e
Slawomir Kasprzak <slawek.kasprzak@icloud.com>
2024-07-25 18:25:11 +0100

    Added model classes

--- src/main/java/com/example/githubapi/model/Repository.java
+++ src/main/java/com/example/githubapi/model/Repository.java
@@ -1,4 +1,33 @@
 package com.example.githubapi.model;
 
+import java.util.List;
+
 public class Repository {
+    private String name;
+    private String ownerLogin;
+    private List<Branch> branches;
+
+    public List<Branch> getBranches() {
+        return branches;
+    }
+
+    public void setBranches(List<Branch> branches) {
+        this.branches = branches;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerLogin() {
+        return ownerLogin;
+    }
+
+    public void setOwnerLogin(String ownerLogin) {
+        this.ownerLogin = ownerLogin;
+    }
 }
